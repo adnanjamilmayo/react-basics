393dfc3def24ae7d46bfc82a042839f0954b7f4f
Adnan <adnanjamilrajpoot@gmail.com>
2023-10-29 01:49:27 +0500

    basic props and tailwind css

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import UI from './components/Ui'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black rounded-xl p-4">Tailwind</h1>
+      <UI username= "Muhammad adnan" />
+      <UI />
+    </>
+  );
+}
+
+export default App;
